f34cb78cadaacb2bd007fa8f7fdac8dc8ddd156a
sleepy2535 <greenapple5657@gmail.com>
2024-10-09 17:16:42 +0900

    하트 제외 완성

--- domi/src/App.js
+++ domi/src/App.js
@@ -5,26 +5,8 @@ import Main from "./pages/Mainpage";
 import Login from "./pages/Login";
 import Posted from "./pages/Posted";
 import Posting from "./pages/Posting";
-<<<<<<< HEAD
 import Payment from "./pages/Payment";
 import Rating from "./pages/Rating";
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/payment" element={<Payment />} />
-          <Route exact path="/posting" element={<Posting />} />
-          <Route exact path="/posted" element={<Posted />} />
-          <Route exact path="/rating" element={<Rating />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-=======
 import Chat from "./pages/Chat";
 function App() {
     return (
@@ -33,14 +15,15 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Main />} />
                     <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/payment" element={<Payment />} />
                     <Route exact path="/posting" element={<Posting />} />
                     <Route exact path="/posted" element={<Posted />} />
+                    <Route exact path="/rating" element={<Rating />} />
                     <Route exact path="/chat" element={<Chat />} />
                 </Routes>
             </Router>
         </div>
     );
->>>>>>> cbcd97ae5a1e80b22994fee3a37b38467216a3f8
 }
 
 export default App;
