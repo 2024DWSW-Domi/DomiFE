458f4c457ec6da0a6dae5e1145b91df28422367f
sleepy2535 <greenapple5657@gmail.com>
2024-10-10 03:35:39 +0900

    애니메이션 효과

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import Posting from "./pages/Posting";
 import Chat from "./pages/Chat";
 import Payment from "./pages/Payment";
 import Rating from "./pages/Rating";
+import LoadingScreen from "./pages/LoadingScreen"; // LoadingScreen import 추가
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/posting" element={<Posting />} />
-          <Route exact path="/posted" element={<Posted />} />
-          <Route exact path="/chat" element={<Chat />} />
-          <Route exact path="/payment" element={<Payment />} />
-          <Route exact path="/rating" element={<Rating />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<LoadingScreen />} />
+                    <Route exact path="/main" element={<Main />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/posting" element={<Posting />} />
+                    <Route exact path="/posted" element={<Posted />} />
+                    <Route exact path="/chat" element={<Chat />} />
+                    <Route exact path="/payment" element={<Payment />} />
+                    <Route exact path="/rating" element={<Rating />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
